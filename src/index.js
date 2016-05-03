8e5f29bf237db609c670ab3ce1368873bfcba8c2
Milosz Frejnik <milosz@frejnik.com>
2016-05-03 23:26:00 +0200

    init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import 'core-js/fn/object/assign';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/Main';
+import HelloWorld from './components/Main';
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
