f5de932b3edf1819d6bf085d278143d4c7494876
Milosz Frejnik <milosz@frejnik.com>
2016-05-04 05:26:06 +0200

    comments

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,22 +1,79 @@
 require('normalize.css/normalize.css');
-require('styles/App.css');
+require('styles/App.scss');
 
 import React from 'react';
 
-let yeomanImage = require('../images/yeoman.png');
 
-class AppComponent extends React.Component {
+class CommentBox extends React.Component {
+
+  _getComments() {
+
+    const commentList = [
+      { id: 1, author: 'Mick Griffin', body: 'What is love ?', avatarUrl: '../images/yeoman.png' },
+      { id: 2, author: 'John Doe', body: 'Baby don\'t hurt me', avatarUrl: '../images/yeoman.png' }
+    ];
+
+    return commentList.map( (comment) => {
+      return (
+        <Comment
+          author={comment.author}
+          body={comment.body}
+          avatarUrl={comment.avatarUrl}
+          key={comment.id}
+        />
+      );
+    });
+  }
+
+
+  _getCommentsTitle(commentCount) {
+    if (commentCount === 1) {
+      return  '1 comment';
+    }
+    else if (commentCount === 0) {
+      return 'No comments yet';
+    }
+    else {
+      return `${commentCount} comments`;
+    }
+  }
+
+
   render() {
+    const comments = this._getComments();
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
+      <div className="comment-box">
+        <h3>Comments</h3>
+        <h4 className="comment-count">
+          {this._getCommentsTitle(comments.length)}
+        </h4>
+        <div className="comment-list">
+          {comments}
+        </div>
       </div>
     );
   }
 }
 
-AppComponent.defaultProps = {
+class Comment extends React.Component {
+  render() {
+    return (
+      <div className="comment">
+        <img src={this.props.avatarUrl} alt={`${this.props.author}'s comment`} />
+        <p className="comment-header">{ this.props.author }</p>
+        <p className="comment-body">
+          { this.props.body }
+        </p>
+        <div className="comment-actions">
+          <a href="#">Delete comment</a>
+        </div>
+        <hr />
+      </div>
+    );
+  }
+}
+
+CommentBox.defaultProps = {
 };
 
-export default AppComponent;
+export default CommentBox;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/Main';
+import CommentBox from './components/Main';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<CommentBox />, document.getElementById('comment-box'));
 
